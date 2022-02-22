653897dbb6d9ed49708e0df732821e1748cacc80
patricklime <s160419092@student.ubaya.ac.id>
2022-02-22 22:48:31 +0700

    Tugas week 3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      // $this->call(CategorySeeder::class);
+      $this->call(CategorySeeder::class);
 
-    //    $this->call(ProductSeeder::class);
+       $this->call(ProductSeeder::class);
 
-    //    $this->call(UserSeeder::class);
+       $this->call(UserSeeder::class);
 
         $this->call(MedicineSeeder::class);
     }
